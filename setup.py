58fb4e378cad3b668875e3f9d444a1b85dab2bc3
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2022-03-05 19:15:01 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="ui",
+    version="0.1.0",
+    description="Ui designign library",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Andre-cmd-rgb/Py-ui-library",
+    author="Andre-Cmd-Rgb",
+    author_email="andreavigolini@outlook.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+    ],
+    packages=["ui"],
+    include_package_data=True,
+    install_requires=["os", "tkinter"],
+    entry_points={
+        "console_scripts": [
+            "ui=ui_lib.__main__:main",
+        ]
+    },
+)
