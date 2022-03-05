58ed8ad750cd12d98a7a6988fe293ca5ca1ffeb4
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2022-03-05 18:58:20 +0100

    Add files via upload

---
+++ ui_lib.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+Ui=[]
+def EditState(editstate):
+        if editstate=="do":
+                edit=True
+        elif editstate=="stop":
+                edit=False
+        elif editstate=="what":
+                if editstate=="do":
+                        return True
+                elif editstate=="stop":
+                        return False
+def EditScene(main):
+        if EditState(main):
+                return True
+        if EditState(main)==False:
+                return False
+class UIEdit:
+        def __init__(UiName):
+                UiNum=None
+                Ui.append(UiName)
+                Ui.append(UiNum)
+
+
+def screen():
+        global window
+        window=tk.Tk()
+def addButtom(ButtomText=None,funcion=None):
+        button = tk.Button(window, text=ButtomText, command = funcion)
+        button.pack()
+
+class UI:
+        def __init__(self):
+                pass
+        def addwindow():
+                WindowUi=Window()
+
+def ciao():
+        print("ciao")
+
+screen()
+addButtom("grhg4f",ciao)
