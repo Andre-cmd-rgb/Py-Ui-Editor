0bd9d5a707c6087867cc52c2e86763baed343868
Andre-cmd-rgb <andreavigolini@gmail.com>
2022-03-06 18:03:54 +0100

    update

--- UiLibrary/__init__.py
+++ UiLibrary/__init__.py
@@ -5,7 +5,29 @@ except ImportError:
     print("tkinter did not import successfully. Please check your setup.")
     exit(1)
 
-from .ui import ui
+from . import utilities as utils
+from .utilities import system_config
+from .dialog import *
+
+from .App import App
+from .Box import Box
+from .ButtonGroup import ButtonGroup
+from .CheckBox import CheckBox
+from .Combo import Combo
+from .Drawing import Drawing
+from .ListBox import ListBox
+from .MenuBar import MenuBar
+from .Picture import Picture
+from .PushButton import PushButton
+from .RadioButton import RadioButton
+from .Slider import Slider
+from .Text import Text
+from .TextBox import TextBox
+from .TitleBox import TitleBox
+from .PushButton import PushButton
+from .Waffle import Waffle
+from .Window import Window
+
 
 
 
